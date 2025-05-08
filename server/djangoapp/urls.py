e8067f2ce3993df387fce6ba2579124a875057ca
kelvine <kchawora@yahoo.com>
2025-05-08 21:27:50 +0200

    registrations

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,11 +7,15 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    path(route='registration', view=views.registration, name='registration'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # path for logout
     path(route='logout', view=views.logout_user, name='logout'),
+
+    # path for register (alias for registration)
+    path(route='register', view=views.registration, name='register'),
 
     # path for dealer reviews view
 
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,6 @@ urlpatterns = [
     path('about', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('manifest.json', serve, {'path': 'manifest.json', 'document_root': os.path.join(settings.BASE_DIR, 'frontend/build')}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
